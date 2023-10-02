de753cc7f7b5036b83714637be3148698db56fec
YoheiFF <146219546+YoheiFF@users.noreply.github.com>
2023-10-02 17:45:41 +0900

    first commit

---
+++ src/test/java/com/studyjava/javastudy/JavastudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyjava.javastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavastudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
