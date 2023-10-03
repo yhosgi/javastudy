70eebc19320fd203cae65744f65fdaf04ef93760
YoheiFF <146219546+YoheiFF@users.noreply.github.com>
2023-10-03 21:03:46 +0900

    ほっしー用ファンクション

--- src/main/java/com/studyjava/javastudy/JavastudyApplication.java
+++ src/main/java/com/studyjava/javastudy/JavastudyApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavastudyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavastudyApplication.class, args);
+		// SpringApplication.run(JavastudyApplication.class, args);
+		hossyStudy();
 	}
 
+	private static void hossyStudy(){
+
+	}
 }
