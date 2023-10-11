405fe50bf3eb98105ea894ed12c8944d1acf4b0b
YoheiFF <146219546+YoheiFF@users.noreply.github.com>
2023-10-11 23:59:13 +0900

    20231011_DB接続の外だし等

--- src/main/java/com/studyjava/javastudy/JavastudyApplication.java
+++ src/main/java/com/studyjava/javastudy/JavastudyApplication.java
@@ -4,65 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Scanner;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class JavastudyApplication {
 
 	public static void main(String[] args) {
-		// whiteStudy();
-		whileStudy2();
-		// SpringApplication.run(JavastudyApplication.class, args);
-		// Connection connection = null;
-		// PreparedStatement preparedStatement = null;
-		// ResultSet resultSet = null;
-
-		// try {
-		// // JDBCドライバのロード
-		// Class.forName("org.sqlite.JDBC");
-
-		// // データベースへの接続
-		// String url =
-		// "jdbc:sqlite:/C:\\Users\\yohei\\OneDrive\\ドキュメント\\JAVA\\javastudy\\src\\main\\resources\\SQL\\Test.db";
-		// // データベースファイルのパス
-		// connection = DriverManager.getConnection(url);
-
-		// System.out.println("SQLiteデータベースに接続しました");
-
-		// // ここでデータベース操作を行います
-
-		// // 例: テーブル内のデータを取得する
-		// String sql = "SELECT * FROM TEST"; // テーブル名を適切に置き換える
-		// preparedStatement = connection.prepareStatement(sql);
-		// resultSet = preparedStatement.executeQuery();
-
-		// while (resultSet.next()) {
-		// int id = resultSet.getInt("No"); // 列名を適切に置き換える
-		// String name = resultSet.getString("Name"); // 列名を適切に置き換える
-
-		// System.out.println("ID: " + id + ", Name: " + name);
-		// }
-
-		// } catch (ClassNotFoundException e) {
-		// e.printStackTrace();
-		// } catch (SQLException e) {
-		// e.printStackTrace();
-		// } finally {
-		// try {
-		// if (resultSet != null)
-		// resultSet.close();
-		// if (preparedStatement != null)
-		// preparedStatement.close();
-		// if (connection != null)
-		// connection.close();
-		// } catch (SQLException e) {
-		// e.printStackTrace();
-		// }
-		// }
+		SpringApplication.run(JavastudyApplication.class, args);
 	}
 
 	private static void hossyStudy() {
@@ -200,7 +147,7 @@ public class JavastudyApplication {
 		int i = 0;
 		int inputValue;
 		while (i < 1) {
-			
+	
 			try {
 				System.out.println("数字を入力してください");
 				inputValue = scan.nextInt();
@@ -211,15 +158,15 @@ public class JavastudyApplication {
 				} else {
 					System.out.println(inputValue + "は10より小さいです");
 				}
-
+				// scan.close(); // この行を削除
 				i++;
-
+	
 			} catch (Exception e) {
 				System.out.println("数字を入力してください2");
-			} 
-		
+				scan.next();
+			}
 		}
-		scan.close();
-	
+		// scan.close(); // 最終的にはここでスキャナーを閉じるべきです
 	}
+	
 }
